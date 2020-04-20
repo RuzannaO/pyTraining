eeea35dc67b5366044e4ddd624c086202648a9c8
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-04-20 20:15:43 +0400

    Update practical1.py

--- week1/practical1.py
+++ week1/practical1.py
@@ -1,1 +1,16 @@
 
+def implications3(a,b,c):
+
+    if a == 0:
+        result = True
+    else:
+        if b == 0:
+            result = True
+        else:
+            if c == 1:
+                result = True
+            else:
+                result = False
+
+    return result
+  
