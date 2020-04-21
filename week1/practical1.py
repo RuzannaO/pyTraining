fb24282aa157d5513af062f5bab93943f92f33c2
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-04-21 15:39:05 +0400

    Update practical1.py

--- week1/practical1.py
+++ week1/practical1.py
@@ -14,3 +14,17 @@ def implications3(a,b,c):
 
     return result
   
+
+    
+    
+def is_polindrome(a):
+    set_a = set(a)
+    counter = 0
+    for i in set_a:
+        if a.count(i)%2==1:
+            counter+=1
+    if counter>1:
+        print(counter,"counter")
+        return False
+    else:
+        return True
