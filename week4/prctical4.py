bba0a8df8d8b9b5829ab81e499badde4cd078095
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-05-10 15:15:29 +0400

    Update prctical4.py

--- week4/prctical4.py
+++ week4/prctical4.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # nice dataframe creation
-indices=["Avagyan Avag","Baghdasaryan Babken","Garanyan Gourgen"]
 df = pd.DataFrame(columns=['Name', 'Sirname', 'sex', 'appx age', 'status'])
 df['Name']=['Arman','Narek','Norik','zarmik']
 df['status']=['student','tutor','student','tutor']
