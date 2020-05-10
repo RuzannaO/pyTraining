7f3fd7ac34aeab0b3c6bd55eb16b9864fc3280f8
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-05-10 20:02:01 +0400

    Update prctical4.py

--- week4/prctical4.py
+++ week4/prctical4.py
@@ -2,10 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# nice dataframe creation
+
 df = pd.DataFrame(columns=['Name', 'Sirname', 'sex', 'appx age', 'status'])
-df['Name']=['Arman','Narek','Norik','zarmik']
+df['Name']=['Arman','Narek','Norik','Zarmik']
 df['status']=['student','tutor','student','tutor']
 df.set_index('status',inplace=True)
 
 print(df)
+
+
+
+df1 = pd.read_csv('netflix_titles.csv',parse_dates = ['date_added'])
+df2=df1[(df1['release_year']>2015) & ((df1["cast"].str.contains('Leonardo DiCaprio'))|(df1["cast"].str.contains('Kevin Spacey')))]
+print(len(df2))
+
+
